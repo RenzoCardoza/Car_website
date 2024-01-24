167d2773ae2d2e032c5f618b136532bbb7dc029a
Renzo Cardoza <darkarcid@hotmail.com>
2024-01-24 20:50:59 -0300

    added some error handler on the server.js

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.get("/", baseController.buildHome)
 app.use("/inv", inventoryRoute)
 
 /* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
