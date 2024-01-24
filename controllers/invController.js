36c78f1cf4a7e5c04fc26390e4afbeeaea43a637
Renzo Cardoza <darkarcid@hotmail.com>
2024-01-23 23:39:04 -0300

    fixed some errors

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,5 +1,5 @@
 const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const utilities = require("../utilities")
 
 const invCont = {}
 
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseControllers")
+const inventoryRoute = require("./routes/inventoryRoute.js")
 
 /* ***********************
  * View Engine and Templates
